9cc6b27e305eade5919ed62bff47971364cac9ca
MrClayPole <9336574+MrClayPole@users.noreply.github.com>
2022-05-02 10:59:48 +0100

    Uploaded latest verison

--- Openstack_storage_billing_report.sh
+++ Openstack_storage_billing_report.sh
@@ -3,8 +3,8 @@
 # Openstack billing report gnocchi storage dump to CSV
 #
 
-start_date=2021-10-01
-end_date=2021-11-01
+start_date=2022-03-01
+end_date=2022-04-01
 
 echo \"Project Name\",\"Project ID\",\"Volume Name\",\"Volume ID\",\"Volume State\",\"Volume Size GB\",\"Volume attached to\",\"Volume GB hours\"
 
@@ -14,7 +14,8 @@ do
   while IFS="," read -r os_vol_id_in_project os_vol_name_in_project os_vol_status_in_project os_vol_size_in_project os_vol_attached_in_project
   do
     os_vol_gb_hours=$(eval openstack metric measures show -r $os_vol_id_in_project volume.size --granularity 300 --resample 3600 -f value -c value --start $start_date --stop $end_date | paste -sd+ - | bc)
-    os_vol_attached_in_project_trimmed=$(eval echo $os_vol_attached_in_project | cut -c17-52)
+#    os_vol_attached_in_project_trimmed=$(echo $os_vol_attached_in_project | sed 's/"//g' | sed "s/'/\"/g" | sed 's/[][]//g' | jq .server_id)
+    os_vol_attached_in_project_trimmed=$(eval echo $os_vol_attached_in_project | cut -c173-208)
 #    os_vol_attached_in_project_trimmed=$(eval echo $os_vol_attached_in_project | sed 's/Attached to //' | sed 's/ on \/dev\/.*//')
     echo \"$os_project_name\",\"$os_project_id\",$os_vol_name_in_project,$os_vol_id_in_project,$os_vol_status_in_project,$os_vol_size_in_project,$os_vol_attached_in_project_trimmed,$os_vol_gb_hours
   done < <(openstack volume list -f csv -c ID -c Name -c Status -c Size -c 'Attached to' --project $os_project_id | tail -n +2)
